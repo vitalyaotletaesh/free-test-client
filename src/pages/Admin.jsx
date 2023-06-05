382cfa51c68049b48648b1e405da1c45a7c9a975
vitalyaotletaesh <xxxamyakov@mail.ru>
2023-06-06 00:22:43 +0300

    новый сервер ура

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Card, Col, Container} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllTests} from "../redux/features/auth/testSlice";
 
 const Admin = () => {
+    const dispatch = useDispatch()
+    const tests = useSelector((state) => state.test.tests)
+
+    useEffect(() => {
+        const params = {
+            limit: 0,
+            page: 0,
+        }
+        dispatch(getAllTests(params))
+    }, [dispatch, tests])
+
     return (
         <Container className="d-flex">
             <Col md={6} className="mt-3">
-                <Card  border={"light"} className="mx-lg-auto">
+                <Card border={"light"} className="mx-lg-auto">
                     <table className="table">
                         <thead>
                         <tr>
@@ -21,16 +34,24 @@ const Admin = () => {
             </Col>
 
             <Col md={6} className="mt-3 m-lg-3">
-                <Card  border={"light"} className="mx-lg-auto">
+                <Card border={"light"} className="mx-lg-auto">
                     <table className="table">
                         <thead>
-                        <tr>
-                            <th>Test name</th>
-                            <th>Author</th>
-                        </tr>
+                        {tests?.length > 0 ?
+                            tests.map((test, index) => (
+                                <tr key={index}>
+                                    <th>{test.name}</th>
+                                    <th>Author</th>
+                                </tr>
+                            ))
+                            :
+                            <tr>
+                                <th>Test name</th>
+                                <th>Author</th>
+                            </tr>
+                        }
                         </thead>
                         <tbody>
-
                         </tbody>
                     </table>
                 </Card>
